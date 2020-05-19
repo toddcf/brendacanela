150d9359874a4d95376f5470d0eda58204f617cc
Todd Croak-Falen <toddcf@gmail.com>
2020-05-18 23:59:24 -0700

    Reduced footer.js code.

---
+++ assets/js/footer.js
@@ -0,0 +1,5 @@
+// Footer Copyright Date:
+var thisYear = new Date().getFullYear();  
+var thruCurrentYear = document.querySelector('.currentYear');
+
+thruCurrentYear.innerHTML = '&ndash;&nbsp;' + thisYear + '&nbsp;';
