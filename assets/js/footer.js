d74c21799e613fe005459fd136a0dddd438bb1d8
Todd Croak-Falen <toddcf@gmail.com>
2020-05-19 18:32:57 -0700

    Ran CSS Autoprefixer.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,5 +1,4 @@
 // Footer Copyright Date:
 var thisYear = new Date().getFullYear();  
 var thruCurrentYear = document.querySelector('.currentYear');
-
 thruCurrentYear.innerHTML = '&ndash;&nbsp;' + thisYear + '&nbsp;';
